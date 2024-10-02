d938b82195a9a474967409fd696185ab8eddffef
vladaliho <vlada.liho@gmail.com>
2024-10-02 14:38:33 +0100

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const reversedString = Math.abs(n).toString().split("").reverse().join("");
+
+    const reversedNumber = parseInt(reversedString, 10);
+
+    return n < 0 ? -reversedNumber : reversedNumber;
+};
